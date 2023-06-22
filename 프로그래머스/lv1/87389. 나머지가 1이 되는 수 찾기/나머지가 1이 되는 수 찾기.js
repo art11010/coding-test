6192798dd1ff926cf5c5d2537c5c67c72943325f
YEZI <yezi.dev@gmail.com>
2023-06-22 16:33:10 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = 0;
+    for(let i = 2; i < n; i++){
+        if((n-1)%i ===0){
+            answer = i;
+            break
+        }
+    }
+    return answer;
+}

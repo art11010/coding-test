deaa6f851bc986203a8251cb1b5b0c0c5065837b
YEZI <yezi.dev@gmail.com>
2023-06-28 13:06:27 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,12 @@
+function solution(num) {
+  var answer = 0;
+  while (num !== 1 && answer < 500) {
+    answer++;
+    if (num % 2 === 0) {
+      num /= 2;
+    } else {
+      num = num * 3 + 1;
+    }
+  }
+  return answer === 500 ? -1 : answer;
+}

3a13895f2e51908e47d342b9a580ac970d1c7db0
YEZI <yezi.dev@gmail.com>
2023-06-30 20:38:45 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
@@ -0,0 +1,8 @@
+function solution(arr, divisor) {
+  const answer = arr
+    .filter((num) => {
+      return num % divisor === 0;
+    })
+    .sort((a, b) => a - b);
+  return answer.length === 0 ? [-1] : answer;
+}
